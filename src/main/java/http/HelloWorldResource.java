6b5ec70d7b8b5337115c69f2970921922874118d
Saif Hatem <s.hatem@yodawy.com>
2022-07-02 12:34:51 +0200

    Added hello endpoint to test opentelemetry

---
+++ src/main/java/http/HelloWorldResource.java
@@ -0,0 +1,19 @@
+package http;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.jboss.logging.Logger;
+
+@Path("/hello")
+public class HelloWorldResource {
+    private static final Logger LOG = Logger.getLogger(HelloWorldResource.class);
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        LOG.info("hello");
+        return "hello";
+    }
+}
