42b3dbe6d39196cf71427ab42fc462ac7b3fca6d
Saif Hatem <s.hatem@yodawy.com>
2022-07-02 12:35:49 +0200

    Make endpoint nonblocking

--- src/main/java/http/HelloWorldResource.java
+++ src/main/java/http/HelloWorldResource.java
@@ -6,11 +6,14 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.jboss.logging.Logger;
 
+import io.smallrye.common.annotation.NonBlocking;
+
 @Path("/hello")
 public class HelloWorldResource {
     private static final Logger LOG = Logger.getLogger(HelloWorldResource.class);
 
     @GET
+    @NonBlocking
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         LOG.info("hello");
