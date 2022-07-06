805e99b4a4246ac23ed54b8f452e0c3116717377
Saif Hatem <s.hatem@yodawy.com>
2022-07-06 23:18:01 +0200

    Added endpoint that uses new rest client

--- src/main/java/http/HelloWorldResource.java
+++ src/main/java/http/HelloWorldResource.java
@@ -1,22 +1,39 @@
 package http;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
 
+import http.services.CatFactService;
 import io.smallrye.common.annotation.NonBlocking;
+import io.smallrye.mutiny.Uni;
+import models.CatFact;
 
 @Path("/hello")
 public class HelloWorldResource {
     private static final Logger LOG = Logger.getLogger(HelloWorldResource.class);
 
+    @Inject
+    @RestClient
+    CatFactService catFactService;
+    
     @GET
     @NonBlocking
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         LOG.info("hello");
         return "hello";
+    }
+
+    @GET
+    @Path("/cats")
+    @NonBlocking
+    public Uni<CatFact> getFact() {
+        return catFactService.get();
     }
 }
