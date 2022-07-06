e417f704c84331eec94c029d316434acfdb75761
Saif Hatem <s.hatem@yodawy.com>
2022-07-06 23:18:54 +0200

    Use new activity that has an http call

--- src/main/java/temporal/workflows/impl/HelloWorldWorkflowImpl.java
+++ src/main/java/temporal/workflows/impl/HelloWorldWorkflowImpl.java
@@ -4,21 +4,25 @@ import java.time.Duration;
 
 import io.temporal.activity.ActivityOptions;
 import io.temporal.workflow.Workflow;
+import models.CatFact;
 import temporal.activities.Capitalizer;
+import temporal.activities.CatFactFetcher;
 import temporal.workflows.HelloWorldWorkflow;
 
 
 public class HelloWorldWorkflowImpl implements HelloWorldWorkflow {
 
     ActivityOptions options = ActivityOptions.newBuilder()
-            .setScheduleToCloseTimeout(Duration.ofSeconds(2))
+            .setScheduleToCloseTimeout(Duration.ofSeconds(15))
             .build();
 
     // ActivityStubs enable calls to Activities as if they are local methods, but actually perform an RPC.
     private final Capitalizer capitalizer = Workflow.newActivityStub(Capitalizer.class, options);
+    private final CatFactFetcher cats = Workflow.newActivityStub(CatFactFetcher.class, options);
 
     @Override
     public String getGreeting(String name) {
+        CatFact catFact = cats.getCatFact();
         return capitalizer.capitalize(name);
     }
 }
